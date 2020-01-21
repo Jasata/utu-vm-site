67c2b3b07c8904375305af54ca9a2b9830ddede2
Jani Tammi <jasata@utu.fi>
2020-01-21 14:46:48 +0200

    Change: Downloads table and host architecture for files

--- create.sql
+++ create.sql
@@ -1,39 +1,64 @@
 CREATE TABLE teacher
 (
-    uid             TEXT        NOT NULL PRIMARY KEY,
-    created         TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    status          TEXT        NOT NULL DEFAULT 'active',
+    uid                 TEXT        NOT NULL PRIMARY KEY,
+    created             TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status              TEXT        NOT NULL DEFAULT 'active',
     CHECK (status IN ('active', 'inactive'))
+);
+CREATE TABLE host_architecture
+(
+    type                TEXT        NOT NULL PRIMARY KEY,
+    description         TEXT        NOT NULL,
+    UNIQUE (description)
 );
 CREATE TABLE file
 (
-    id              INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
-    name            TEXT        NOT NULL UNIQUE,
-    size            INTEGER     NOT NULL,
-    sha1            TEXT            NULL,
-    type            TEXT        NOT NULL,
-    label           TEXT        NOT NULL,
-    version         TEXT        NOT NULL DEFAULT (strftime('%Y-%m-%d', 'now')),
-    ostype          TEXT            NULL,
-    description     TEXT            NULL,
-    ram             TEXT            NULL,
-    cores           TEXT            NULL,
-    disksize        TEXT            NULL,
-    dtap            TEXT        NOT NULL DEFAULT 'production',
-    created         TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    owner           TEXT        NOT NULL,
-    downloadable_to TEXT        NOT NULL DEFAULT 'nobody',
+    id                  INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name                TEXT        NOT NULL UNIQUE,
+    size                INTEGER     NOT NULL,
+    sha1                TEXT            NULL,
+    type                TEXT        NOT NULL,
+    label               TEXT        NOT NULL,
+    version             TEXT        NOT NULL DEFAULT (strftime('%Y-%m-%d', 'now')),
+    host_architecture   TEXT            NULL,
+    ostype              TEXT            NULL,
+    description         TEXT            NULL,
+    ram                 TEXT            NULL,
+    cores               TEXT            NULL,
+    disksize            TEXT            NULL,
+    dtap                TEXT        NOT NULL DEFAULT 'production',
+    created             TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    owner               TEXT        NOT NULL,
+    downloadable_to     TEXT        NOT NULL DEFAULT 'nobody',
     FOREIGN KEY (owner) REFERENCES teacher (uid),
-    UNIQUE(label, version, type),
-    CHECK (type IN ('usb', 'vm')),
+    FOREIGN KEY (host_architecture) REFERENCES host_architecture (type),
+    UNIQUE (label, version, type),
+    CHECK (type IN ('usb', 'vm', 'sd')),
+    CHECK ((type = 'vm' AND host_architecture IS NULL) OR (type != 'vm' AND host_architecture IS NOT NULL)),
     CHECK (dtap IN ('development', 'testing', 'acceptance', 'production')),
     CHECK (downloadable_to IN ('anyone', 'student', 'teacher', 'nobody'))
 );
-CREATE TABLE uses
+CREATE TABLE course
 (
-    uid             TEXT        NOT NULL,
-    id              INTEGER     NOT NULL,
-    PRIMARY KEY (uid, id),
-    FOREIGN KEY (uid) REFERENCES teacher (uid),
-    FOREIGN KEY (id)  REFERENCES file (id) ON DELETE CASCADE
+    code                TEXT        NOT NULL PRIMARY KEY,
+    name                TEXT        NOT NULL UNIQUE,
+    teacher             TEXT        NOT NULL,
+    FOREIGN KEY (teacher) REFERENCES teacher (uid) ON DELETE CASCADE,
+    CHECK (code = upper(code))
+);
+CREATE TABLE uses_for
+(
+    teacher             TEXT        NOT NULL,
+    file_id             INTEGER     NOT NULL,
+    course_code         TEXT        NOT NULL,
+    PRIMARY KEY (teacher, file_id, course_code),
+    FOREIGN KEY (teacher) REFERENCES teacher (uid) ON DELETE CASCADE,
+    FOREIGN KEY (file_id) REFERENCES file (id),
+    FOREIGN KEY (course_code) REFERENCES course (code) ON DELETE CASCADE
+);
+CREATE TABLE downloads
+(
+    datetime            TEXT        NOT NULL,
+    filename            TEXT        NOT NULL,
+    size                INTEGER     NOT NULL
 );
--- insert_dev.sql
+++ insert_dev.sql
@@ -6,19 +6,24 @@ VALUES
 ('apmake'),
 ('tianyl'),
 ('sjprau');
-INSERT INTO file (label, version, size, sha1, name, type, owner, downloadable_to)
+INSERT INTO host_architecture
 VALUES
-("Java (generic)", "356", "1996267520", NULL, "utuvm-java-356.ova", "vm", "jmjmak", "teacher"),
-("Java (generic)", "420", "2002186240", NULL, "utuvm-java-420.ova", "vm", "jmjmak", "anyone"),
-("Java (generic)", "424", "2004142080", NULL, "utuvm-java-424.ova", "vm", "jmjmak", "anyone"),
-("LaTeX / LyX / Pandoc", "412", "2119680000", NULL, "utuvm-latex-412.ova", "vm", "jmjmak", "teacher"),
-("LaTeX / LyX / Pandoc", "422", "2185441280", NULL, "utuvm-latex-422.ova", "vm", "jmjmak", "anyone"),
-("C/C++/Web/Mobile/Data Science/Declarative", "356", "1504153600", NULL, "utuvm-minimega-356.ova", "vm", "tumipo", "student"),
-("C/C++/Web/Mobile/Data Science/Declarative", "420", "1516462080", NULL, "utuvm-minimega-420.ova", "vm", "tumipo", "anyone"),
-("Java (generic)", "308", "2087362670", NULL, "utuvm-java-308.zip", "usb", "apmake", "student"),
-("Java (generic)", "356", "2216374659", NULL, "utuvm-usb-java-356.zip", "usb", "apmake", "student"),
-("Java (generic)", "420", "2348810240", NULL, "utuvm-usb-java-420.img", "usb", "tianyl", "anyone"),
-("Java (generic)", "424", "2350907392", NULL, "utuvm-usb-java-424.img", "usb", "tianyl", "anyone"),
-("C/C++/Web/Mobile/Data Science/Declarative", "356", "1726122925", NULL, "utuvm-usb-minimega-356.zip", "usb", "tianyl", "student"),
-("C/C++/Web/Mobile/Data Science/Declarative", "420", "1865416704", NULL, "utuvm-usb-minimega-420.img", "usb", "jmjmak", "anyone");
+('amd64', 'Intel 64-bit'),
+('arm', 'ARM 64/32-bit'),
+('riscv', 'RISC-V');
+INSERT INTO file (label, version, size, sha1, name, type, host_architecture, owner, downloadable_to)
+VALUES
+("Java (generic)", "356", "1996267520", NULL, "utuvm-java-356.ova", "vm", NULL, "jmjmak", "teacher"),
+("Java (generic)", "420", "2002186240", NULL, "utuvm-java-420.ova", "vm", NULL, "jmjmak", "anyone"),
+("Java (generic)", "424", "2004142080", NULL, "utuvm-java-424.ova", "vm", NULL, "jmjmak", "anyone"),
+("LaTeX / LyX / Pandoc", "412", "2119680000", NULL, "utuvm-latex-412.ova", "vm", NULL, "jmjmak", "teacher"),
+("LaTeX / LyX / Pandoc", "422", "2185441280", NULL, "utuvm-latex-422.ova", "vm", NULL, "jmjmak", "anyone"),
+("C/C++/Web/Mobile/Data Science/Declarative", "356", "1504153600", NULL, "utuvm-minimega-356.ova", "vm", NULL, "tumipo", "student"),
+("C/C++/Web/Mobile/Data Science/Declarative", "420", "1516462080", NULL, "utuvm-minimega-420.ova", "vm", NULL, "tumipo", "anyone"),
+("Java (generic)", "308", "2087362670", NULL, "utuvm-java-308.zip", "usb", "amd64", "apmake", "student"),
+("Java (generic)", "356", "2216374659", NULL, "utuvm-usb-java-356.zip", "usb", "amd64", "apmake", "student"),
+("Java (generic)", "420", "2348810240", NULL, "utuvm-usb-java-420.img", "usb", "amd64", "tianyl", "anyone"),
+("Java (generic)", "424", "2350907392", NULL, "utuvm-usb-java-424.img", "usb", "amd64", "tianyl", "anyone"),
+("C/C++/Web/Mobile/Data Science/Declarative", "356", "1726122925", NULL, "utuvm-usb-minimega-356.zip", "usb", "amd64", "tianyl", "student"),
+("C/C++/Web/Mobile/Data Science/Declarative", "420", "1865416704", NULL, "utuvm-usb-minimega-420.img", "usb", "amd64", "jmjmak", "anyone");
 
